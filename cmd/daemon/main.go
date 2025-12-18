222b9f34d2f96221d0be2896f0f37bdad212c532
Nathan Brown <32497422+KingOfTac@users.noreply.github.com>
2025-12-17 21:00:42 -0700

    add daemon implementation

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/apparentlymart/go-userdirs/userdirs"
+	"github.com/kingoftac/gork/internal/db"
+	"github.com/kingoftac/gork/internal/scheduler"
+	"github.com/kingoftac/gork/internal/version"
+)
+
+var (
+	dbName = "gork.db"
+	dirs   = userdirs.ForApp("gork", "com.github.kingoftac.gork", "com.github.kingoftac.gork")
+	dbPath = dirs.DataHome() + string(os.PathSeparator) + dbName
+)
+
+func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}))
+	slog.SetDefault(logger)
+
+	db, err := db.NewDB(dbPath)
+	if err != nil {
+		slog.Error("failed to open database", "error", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	sched := scheduler.NewScheduler(db)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigCh
+		slog.Info("Received shutdown signal, exiting...")
+		cancel()
+	}()
+
+	slog.Info("Starting gork daemon...", "version", version.Version)
+	sched.Start(ctx)
+	slog.Info("Gork daemon stopped")
+}
